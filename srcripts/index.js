49b954cb4bf5525b4cf83d554f8bc18f7eadcde3
freshjspd <freshjs.pd@gmail.com>
2023-02-12 12:29:46 +0200

    условия. логические операторы

--- srcripts/index.js
+++ srcripts/index.js
@@ -1,128 +1,24 @@
-/*
-if(условие){
-    // code
-} else{
-    // code если условие не выполнилось
+// логические операторы для условия
+// && (and,и) выполняется и 1условие и 2 условие одновренно
+// || (or,или) выполняется или 1условие или 2 условие одновренно, хотя бы одно из
+
+// ot 0 do 10 ???
+
+if(x>0 && x<10){
+    console.log('ot 0 do 10');
 }
 
-операторы сравления  >   <   >=   <=   ==   !=
-*/
+// женский пол, соверш.
+if(isMale==false && age >= 18){}
 
-// просим ввести число и узнать больше оно 10
+// 2 четверть часа
+if( m >= 15  &&  m<30 ) {}
 
-/*
-let x;
-x = prompt('Enter value x');
-x = Number(x);
+// юзер зарегестрирован и баланс карты больше sum и
+if(isAuth == true && cardBalance >= sum  ) {}
 
-if(isNaN(x)== false){
-
-    if(x >= 10){
-        console.log('Yes. X >= 10');
-    } else{
-        console.log('No. X < 10');
-    }
-
-} else{
-    console.log('Ошибка. Введенное значение не число');
-}
-*/
-
-/*
-    if(условие1){
-        //code при выполнение условия1
-    } else if(условие2){
-        //code при выполнение условия2
-    } else if(условие3){
-        //code при выполнение условия3
-    } else{
-        // code если ни одно условие не было выполнено
-    }
-*/
-/*
-
-// задача. просим ввести число и узнать больше оно 10 или 100
-
-if(x < 10){
-    console.log('x<10');
-} else if (x > 100){
-    console.log('x>100');
-} else{
-    console.log('x в диапазоне от 10 до 100')
-}
-
-// task. Пользователь вводит число минут. определить в
-//какую четверть часа мы попали
-let valMinutes = prompt('Введите количество минут');
-if(valMinutes <= 15){
-    console.log('1 part of hour');
-} else if(valMinutes <= 30){
-    console.log('2 part of hour');
-} else if(valMinutes <= 45){
-    console.log('3 part of hour');
-} else {
-    console.log('4 part of hour');
-}
-
-// task. пользователь вводит номер времени года (1 - зима, 2 -весна, 3 - лето, 4 - осень)
-// вывести название времени года по числу
-let numMo = prompt('Введете номер времени года');
-if(numMo == 1){
-    console.log("It's winter");
-} else if(numMo == 2){
-    console.log("It's spring");
-} else if(numMo == 3){
-    console.log("It's summer");
-} else if(numMo == 4){
-    console.log("It's autumn");
-} else{
-    console.log("Error");
-}
-
-// task. пользователь вводит возраст. проверить совершеннолетний ли он?
-let userAge = prompt('Ваш возраст?');
-if(userAge >= 18) {
-    console.log('совершеннолетний');
-} else{
-    console.log('несовершеннолетний');
-}
-
-
-// проверить пользователя знает ли он сколько будет 1+1
-let answerUser = prompt('1+1=????');
-let answerComp = 2;
-if(answerUser == answerComp) {
-    console.log('You are right! Bingo!)');
-} else{
-    console.log('Sorry. 1+1=2 !');
-}
-
-
-*/
-// товар стоит 120 грн. пользователь покупает определенное количество штук
-// подсчитать стоимость покупки
-// скидку 3% если сумма больше 700 грн
-// суидку 5% если сумма больше 1000 грн
-
-let productPrice = 120;
-let productCount = Number(prompt('Введите количество товара',1));
-let sum;
-let sale = 0;
-sum = productPrice * productCount;
-if(sum >= 1000) {
-    sale = 5;
-    sum = sum * (100 - sale)/100;
-} else if(sum >= 700){
-    sale = 3;
-    sum = sum * (100 - sale)/100;
-}
-console.log('Сумма к оплате ', sum, 'грн. скидка:', sale, '%');
-
-
-
-
-
-
-
+// в базу может добавить запись юзер или админ или модератор
+// role
+if(role == 'admin' || role == 'moderator') {}
 
 
