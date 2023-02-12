bb200cfa01455a41c0f0b51737bb924308c3adcd
freshjspd <freshjs.pd@gmail.com>
2023-02-12 12:00:20 +0200

    условия

--- srcripts/index.js
+++ srcripts/index.js
@@ -1,161 +1,126 @@
-console.log("Hello, World!!!");
-
-//alert("Hello, Java Script!!");
-
-// однострочный комментарий
-
 /*
-многострочный комент
-и еще
-и еще
+if(условие){
+    // code
+} else{
+    // code если условие не выполнилось
+}
+
+операторы сравления  >   <   >=   <=   ==   !=
 */
 
-/******************** */
-// обьявление переменных
-let name = "Julia"; // string
-let year = 2023; // number
-let month = "Feb"; //string
-let a = 10,
-  b = 20,
-  c = 30; // number
-let pi = 3.141596; //number
-let user1 = "Vasya Testovich",
-  user2 = "Masha Fox",
-  user3 = "Tom Adson"; // string
-let isMale = false; // boolean
-let isWeekend = true; // boolean
-
-// вывод переменных
-console.log(name);
-console.log(year);
-console.log(month, year);
-console.log("Month is", month, "and year is", year);
-
-console.log("Sum a and b and c is", a + b + c);
-
-// работа с переменными
-let x = 100;
-x = 1;
-x = 0;
-x = 9;
-console.log(x);
-x = "hello";
-console.log(x);
-x = 200;
-console.log(x);
-x = "Tom";
-x = "John Smith";
-console.log(x);
-
-let y = 5,
-  y2 = 20;
-x = y;
-console.log("x=", x, "y=", y);
-x = y2;
-console.log("x=", x, "y=", y);
-
-// арифм операции
-x = 10;
-x = 10 + 4 * 5 - 2 + (3 + 4) / 2;
-console.log(x);
-x = 10;
-y = 5;
-let result;
-result = x + y;
-console.log(result);
-result = x - y;
-console.log(result);
-result = x * y;
-console.log(result);
-result = x / y;
-console.log(result);
-
-x = x + 2;
-console.log(x);
-x = x - 2;
-console.log(x);
-
-// инкремент
-x = x + 1;
-console.log("инкремент", x);
-x++;
-console.log("инкремент", x);
-// декремент
-x = x - 1;
-console.log("декремент", x);
-x--;
-console.log("декремент", x);
-
-/* практика
-
-1 Определите переменные со значениями 11, true, "java script", "100" 
-и выведите их в консоль
-2. Создайте переменные greeting со значением'Hello, ' и userName. 
-Напишити имя пользователя в переменную userName. 
-С помощью диалога выведите сообщение с приветствием. Например, 'Hello, Vasya!'.
-3 Создайте переменные a=10 и b=20. 
-Отнимите от a переменную b и результат присвойте переменной c. 
-Затем создайте переменную d, присвойте ей значение c. 
-Сложите переменные c и d, а результат запишите в переменную result.
-
-*/
-
-// task1
-let x1 = 11;
-let x2 = true;
-let x3 = "java script",
-  x4 = "100";
-console.log(x1, x2, x3, x4);
-
-// task2
-let greeting = "Hello, ";
-let userName = "Vasya";
-console.log(greeting, userName, "!!!!");
-//alert(greeting+userName+"!!!!");
-
-// task3
-let varA = 10,
-  varB = 20;
-let varC = varA - varB;
-let varD = varC;
-let res;
-res = varC + varD;
-console.log(varA, varB, varC, varD, res);
+// просим ввести число и узнать больше оно 10
 
 /*
-task 4. написать программу перевода метры в сантиметры
-task 5. подчитать площать прямоугольника (s = a *b)
+let x;
+x = prompt('Enter value x');
+x = Number(x);
+
+if(isNaN(x)== false){
+
+    if(x >= 10){
+        console.log('Yes. X >= 10');
+    } else{
+        console.log('No. X < 10');
+    }
+
+} else{
+    console.log('Ошибка. Введенное значение не число');
+}
 */
 
 /*
-// task4
-let valM = 20;
-let valCM;
-valCM = valM * 100;
-console.log('task4 ',valCM);
-
-// task5
-let sizeA, sizeB, square;
-sizeA = 10.3;
-sizeB = 20.7;
-square = sizeA * sizeB;
-console.log('task5 ',square);
+    if(условие1){
+        //code при выполнение условия1
+    } else if(условие2){
+        //code при выполнение условия2
+    } else if(условие3){
+        //code при выполнение условия3
+    } else{
+        // code если ни одно условие не было выполнено
+    }
 */
+/*
 
-// tasks with prompt
-let test;
-test = prompt("Введи значение переменной test");
-console.log("Test is ", test);
+// задача. просим ввести число и узнать больше оно 10 или 100
 
-// task4
-let valM = prompt("Введи значение в метрах");
-let valCM;
-valCM = valM * 100;
-console.log("task4 ", valCM, "cm");
+if(x < 10){
+    console.log('x<10');
+} else if (x > 100){
+    console.log('x>100');
+} else{
+    console.log('x в диапазоне от 10 до 100')
+}
 
-// task5
-let sizeA, sizeB, square;
-sizeA = prompt("Введи значение ширины");;
-sizeB = prompt("Введи значение длины");;
-square = sizeA * sizeB;
-console.log("task5 ", square);
+// task. Пользователь вводит число минут. определить в
+//какую четверть часа мы попали
+let valMinutes = prompt('Введите количество минут');
+if(valMinutes <= 15){
+    console.log('1 part of hour');
+} else if(valMinutes <= 30){
+    console.log('2 part of hour');
+} else if(valMinutes <= 45){
+    console.log('3 part of hour');
+} else {
+    console.log('4 part of hour');
+}
+
+// task. пользователь вводит номер времени года (1 - зима, 2 -весна, 3 - лето, 4 - осень)
+// вывести название времени года по числу
+let numMo = prompt('Введете номер времени года');
+if(numMo == 1){
+    console.log("It's winter");
+} else if(numMo == 2){
+    console.log("It's spring");
+} else if(numMo == 3){
+    console.log("It's summer");
+} else if(numMo == 4){
+    console.log("It's autumn");
+} else{
+    console.log("Error");
+}
+
+// task. пользователь вводит возраст. проверить совершеннолетний ли он?
+let userAge = prompt('Ваш возраст?');
+if(userAge >= 18) {
+    console.log('совершеннолетний');
+} else{
+    console.log('несовершеннолетний');
+}
+
+
+// проверить пользователя знает ли он сколько будет 1+1
+let answerUser = prompt('1+1=????');
+let answerComp = 2;
+if(answerUser == answerComp) {
+    console.log('You are right! Bingo!)');
+} else{
+    console.log('Sorry. 1+1=2 !');
+}
+
+
+*/
+// товар стоит 120 грн. пользователь покупает определенное количество штук
+// подсчитать стоимость покупки
+// скидку 3% если сумма больше 700 грн
+// суидку 5% если сумма больше 1000 грн
+
+let productPrice = 120;
+let productCount = Number(prompt('Введите количество товара',1));
+let sum;
+let sale = 0;
+sum = productPrice * productCount;
+if(sum >= 1000) {
+    sale = 5;
+    sum = sum * (100 - sale)/100;
+} else if(sum >= 700){
+    sale = 3;
+    sum = sum * (100 - sale)/100;
+}
+console.log('Сумма к оплате ', sum, 'грн. скидка:', sale, '%');
+
+
+
+
+
+
+
