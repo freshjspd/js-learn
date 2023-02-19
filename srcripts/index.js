600200efe218fa976a8844f435a04f4740abf905
freshjspd <freshjs.pd@gmail.com>
2023-02-19 12:35:20 +0200

    массивы обьявление

--- srcripts/index.js
+++ srcripts/index.js
@@ -1,44 +1,25 @@
-console.log('1:' , sum1(2,2));
-// console.log('2:' , sum2(2,2)); !!!Error
-
-function sum1(a,b){
-    return a+b;
+function greeting(name){
+    console.log('Hello, ' , name, "!!!");
 }
 
-let sum2 = function(a,b){
-    return a+b;
+let users = ['Vasya' , 'Tom', 'Ann', "John"];
+
+for(let i=0; i<users.length; i++){
+    greeting(users[i]);
 }
 
+//---------------------------------
 
-console.log('-------------------');
-console.log('1:' , sum1(2,2));
-console.log('2:' , sum2(2,2));
+let array1 = [1,6,2,3,-5,2,3.14, 2,3,10,-4,5];
+let n1 = array1.length;
 
-// стрелочные функцию
-let sum3 = (a,b) => a+b;
-
-let sum4 = (a,b) => {
-    let result = a+b;
-    return result;
+for(i=0; i<array1.length; i=i+2){
+    console.log(array1[i]);
 }
 
-console.log('3:' , sum3(2,2));
-console.log('4:' , sum4(2,2));
-
-
-/*
-easy calc with array functions
-*/
-
-const sum = (a,b) => a+b;
-const sub = (a,b) => a-b;
-const divi = (a,b) => a/b;
-const mult = (a,b) => a*b;
-
-
-let x1 = Number(prompt('Enter 1 value'));
-let x2 = Number(prompt('Enter 2 value'));
-console.log( sum(x1,x2));
-console.log( sub(x1,x2));
-console.log( divi(x1,x2));
-console.log( mult(x1,x2));
+//sum of array
+let sum = 0;
+for(i=0; i<array1.length; i++){
+    sum = sum + array1[i];
+}
+console.log('Sum of array is ', sum);
