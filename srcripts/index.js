bcf92086bb7800375e2e8423d3994cf5ff6ddcc5
freshjspd <freshjs.pd@gmail.com>
2023-02-25 13:01:57 +0200

    array practice

--- srcripts/index.js
+++ srcripts/index.js
@@ -1,84 +1,45 @@
-let a = [3,10,20,30,40,50], result; 
+// 1,2,3,4,5
+// 10,20,30
 
-/*
-//1
-console.log('-----------');
-a.forEach( el => console.log(el));
-//2
-console.log('-----------');
-a.forEach( (el, i) => console.log(el, ':', i));
-//3
-console.log('-----------');
-function out(el, i, arr){
-    console.log(i,el);
+//result :  5,4,3,2,1,10,20,30  => 5,4,3,2,1,-1,30
+//result :  "5,4,3,2,1,10,20,30"
+
+let a1 = [1,2,3,4,5], a2 = [10,20,30];
+
+console.log(a1.reverse().concat(a2));
+console.log( a1.reverse().concat(a2).splice(5,2,-1) );
+
+result =  a1.reverse().concat(a2);
+a1.splice(5,2,-1);
+console.log(result);
+
+function task1(a1, a2){
+    let res = [] , i, j, res2 = [];
+    for(i=0; i<a1.length; i++){
+        res[i] = a1[a1.length - i -1];
+    }
+    for(j=0; j<a2.length; j++, i++){
+        res[i] = a2[j];
+    }
+    for(i=0, j=0; i<res.length; i++){
+        if(i>=5 && i<=6) continue;
+        res2[j] = res[i];
+        j++;
+    }
+    return res2;
 }
 
-a.forEach(out);
-*/
+// создать функцию которая возвращает обьдинение массивов без 0 и отриц элементов
 
-// умножить элементы массива на 2
-//forEach
-a.forEach(el => console.log(el*2));
-console.log(a);
-//map
-result = a.map(el => el*2);
-console.log(result);
-
-//ищем четные элементы массива
-//find
-result = a.find(el => (el%2 == 0));
-console.log(result);
-//filter
-result = a.filter(el => (el%2 == 0));
-console.log(result);
-//findIndex
-result = a.findIndex(el => (el%2 == 0));
-console.log(result);
-
-
-
-//проверить положительные ли элементы массива 
-//some
-console.log( a.some(el => el>0) );
-//every
-console.log( a.every(el => el>0) );
-
-
-//проверить на четность массив
-
-//1 variant
-result = a.every(el => el%2 == 0);
-// 2 variant
-
-result = a.every(isEven);
-
-function isEven(el, i, arr){
-    return (el%2 == 0) ? true : false;
+function task2(arr1, arr2){
+    return arr1.concat(arr2).filter(el => el > 0);
 }
 
-// concat
-console.log([1,2,3].concat([-1,-2,-3,-4]));
-// join
-console.log([192,156,34,12].join('.'));
-console.log(['S123A','1045','D2300','0012'].join('-'));
+[0,1,2,3,4,5,6,7,8,9]
+index1 = 2;
+index2 = 4;
 
+a.splice(index1, 0 , a.splice(index2));
+a.splice(index1 , 0, [0,1,2,3]);
 
-// sum 
-let sum=0;
-for(let i=0; i<a.length; i++){
-    sum += a[i];
-}
-console.log(sum);
-
-// new!!!!!
-//сумма массива умноженного на 2
-result = a.reduce( (summa, el) => summa + el*2);
-console.log(result);
-
-
-
-let w = ['angel', 'clown', 'mandarin', 'sturgeon', 'x1', 'x2'];
-result = w.splice(2, 2, 'hello');
-console.log(w);
-
-
+0,1,   4,5,6,7,8,9, 2,3,4,5,6,7,8,9
