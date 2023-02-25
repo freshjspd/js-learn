d52ba556e6e5b7cb0e9e2eaa8d5888ef7cd3d876
freshjspd <freshjs.pd@gmail.com>
2023-02-25 09:40:58 +0200

    hometasks review

--- srcripts/index.js
+++ srcripts/index.js
@@ -1,97 +1,36 @@
-/*
-function greeting(name){
-    console.log('Hello, ' , name, "!!!");
-}
+let a = [1,4,-4,3,8,-1, -6, 0, 0, -3, 2, -9, 11, 0, 2], i;
+let sumNeg=0, countNeg=0;
+let arrNegEl = [] , j =0 ;
 
-let users = ['Vasya' , 'Tom', 'Ann', "John"];
-
-for(let i=0; i<users.length; i++){
-    greeting(users[i]);
-}
-
-//---------------------------------
-
-function sumArray(a){
-    let sum = 0;
-    for(let i=0; i<a.length; i++){
-        sum = sum + a[i];
-    }
-    return sum;
-}
-
-
-
-let array1 = [-1,6,2,3,-5,2,3.14, 2,3,10,-4,5];
-let array2 = [1,2,3,4];
-let array3 = [];
-console.log('Sum of array1 is ', sumArray(array1));
-console.log('Sum of array2 is ', sumArray(array2));
-console.log('Sum of array3 is ', sumArray(array3));
-
-// task1 
-// функция которая возвращает только положительные элементы массива
-
-function task1(array){
-    let res = [], j=0;
-    for(let i=0; i<array.length; i++){
-        if(array[i] > 0){
-            res[j] = array[i];
-            j++;
-        }
-    }
-    return res;
-}
-
-console.log(task1(array1));
-
-// task2
-//сумма четных элементов массива
-function sumEvenArray(a){
-    let sum = 0;
-    for(let i=0; i<a.length; i++){
-        (a[i]%2 == 0) ? sum = sum + a[i] : sum;
-    }
-    return sum;
-}
-
-// task3
-// слияние двух массивов в один
-/*[1,2,3,4] and [10,20,30]  => [1,2,3,4,10,20,30]*/ 
-/*
-function concatArrays(array1, array2){
-    let n1=array1.length;
-    let n2=array2.length;
-    for(let i=0; i<n2; i++){
-        array1[n1 + i] = array2[i];
-    }
-    return array1;
-}
-
-console.log( concatArrays([1,2,3,4], [10,20,30,40,50]));
-*/
-
-// сумма двух массивов
-function sumArray(array1, array2){
-    let res = [];
-    for(let i=0; i<array1.length; i++){
-        console.log(i, array1[i], array2[i]);
-        res[i] = array1[i] + array2[i];
-    }
-    return res;
-}
-console.log( sumArray([1,2,3], [10,20,30]));
-console.log( sumArray([1,2,3, 4, 5], [10,20,30]));
-console.log( sumArray([1,2,3], [10,20,30, 40, 50]));
-
-// удалить нули из массива
-function delZerosFromArray(array){
-    let res = [], j=0;
-    for(i=0; i<array.length; i++){
-        if(array[i] == 0) continue;
-        res[j] = array[i];
+for(i=0; i<a.length; i++){
+    if(a[i] < 0){
+        sumNeg = sumNeg + a[i];
+        countNeg++;
+        a[i] = 0;
+        arrNegEl[j] = i;
         j++;
     }
+}
+
+console.log(a);
+console.log('Sum neg el ',sumNeg);
+console.log('Count neg el ', countNeg);
+console.log(arrNegEl);
+
+function task5(a,n){
+    let res = [];
+    for(let i=0; i<n; i++){
+        res[i] = a[i];
+    }
     return res;
+}
+
+function task8(arr, index1, index2){
+    let buf;
+    buf = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = buf;
+    return arr;
 }
 
 
