7b62119d8384d83fc4c6a8e4ba8744473f994564
freshjspd <freshjs.pd@gmail.com>
2023-03-04 13:28:28 +0200

    классы привет

--- srcripts/index.js
+++ srcripts/index.js
@@ -4,6 +4,9 @@ class Client{
         this.fullname = fullname;
         this.tel = tel;
     }
+    printPersonalInfo(){
+        console.log(this.fullname, this.tel);
+    }
 }
 
 // класс наследника
@@ -13,8 +16,36 @@ class User extends Client{
         this.login = login;
         this.password = password;
     }
+    printAuth(){
+        console.log("login:",this.login,"password:",this.password);
+    }
 };
 
 let user1 = new User('vasya1', 'qwerty', 'Vasya Vasya', '1928371984');
+user1.printPersonalInfo();
+user1.printAuth();
+
+
+
+class Calc{
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+    }
+    add(){
+        return this.x+this.y;
+    }
+    sub(){
+        return this.x+this.y;
+    }
+    incX(step=1){
+        return this.x = this.x + step;
+    }
+}
+
+let obj = Calc(2,6);
+obj.add();
+obj.incX();
+obj.incX(100);
 
 
