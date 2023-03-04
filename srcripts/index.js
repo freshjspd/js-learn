578022b7e2e560f3891d234962543ef51271981e
freshjspd <freshjs.pd@gmail.com>
2023-03-04 13:13:54 +0200

    классы наследование

--- srcripts/index.js
+++ srcripts/index.js
@@ -1,23 +1,46 @@
-class Engine{
-    constructor(name, power){
-        this.name = name;
-        this.power = power;
+class User{
+    #login;
+    #password;
+    constructor(login, password, fullName="anov"){
+        this.#login = login;
+        this.#password = password;
+        this.fullName = fullName;
+    }
+    set login(value){
+        this.login = value;
+    }
+    set password(value){
+        this.password = value;
+    }
+    get login(){
+        return this.login;
+    }
+    get password(){
+        return this.password;
     }
 }
-class Car{
-    constructor(color, year, model, engine){
-        this.color = color;
-        this.year = year;
-        this.model = model;
-        this.engine = engine;
+
+class User2{
+    constructor(login, password, fullName="anov"){
+        this._login = login;
+        this._password = password;
+        this.fullName = fullName;
+    }
+    set login(value){
+        this._login = value;
+    }
+    set password(value){
+        this._password = value;
+    }
+    get login(){
+        return this._login;
+    }
+    get password(){
+        return this._password;
     }
 }
 
-
-let engine1 = new Engine('enXXX', 2.0);
-let car1 = new Car("Gray", 2004, "Skoda", engine1);
-console.log(car1);
-
-//let car2 = new Car("Gray", 2004, "Skoda", new Engine('enXXX', 2.0));
+let user1 = new User("tom123", "qwerty");
+console.log(user1);
 
 
