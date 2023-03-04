e184b0ded30cdb9ce0eb62ceb5cf5e52f675b6e7
freshjspd <freshjs.pd@gmail.com>
2023-03-04 11:58:54 +0200

    object practice

--- srcripts/index.js
+++ srcripts/index.js
@@ -1,64 +1,66 @@
-let x = 100;
-let y = x;
-console.log(x,y);
-x = 500;
-console.log(x,y);
+// обьект покупка  + дата покупки, клиент
+// название товара, количесво товара, цена за 1 единицу
 
-let user1 = {name: 'Vasya'};
-let user2 = user1;
-console.log(user1,user2);
-user1.name = "Tom";
-console.log(user1,user2);
-user2.name = "Nike";
-console.log(user1,user2);
+// 1 подсчитать стоимость покупки
+// 2 начислить скидку 5% если сумма итоговая больше 1000 грн
 
-console.log('-----------------');
-let user3 = {name: 'Vasya'};
-let user4 = Object.assign({}, user3);
-console.log(user3, user4);
-user3.name = "Tom";
-console.log(user3, user4);
-user4.name = "Nike";
-console.log(user3, user4);
-user3.age = 20;
-console.log(user3, user4);
+let order = {
+    date: '01-01-2023', 
+    customer: 'Tomas',
+    product: 'pen',
+    count: 1000,
+    price: 12.5,
+    sum: 0,
+    totalSum: function(){
+        this.sum = this.price * this.count;
+        return this.sum;
+    },
+    sale: function(limit, prSale){
+        if(this.sum >= limit){
+            this.sum *= (100-prSale)/100;
+        }
+        return this.sum;
+    }    
 
-console.log('-----------------');
-let user5 = {name: 'Vasya'};
-let user6 = {age: 20, isMale: true};
-let user7 = {};
-Object.assign(user7, user5, user6);
-console.log(user7);
+}
+
+order.sum();
+order.sale(1000,5);
+
+
+
 
 /*
-console.log('-----------------');
-let user8 = {name: 'Vasya'};
-let user9 = {name: 'Tom', age: 20, isMale: true};
-let user10 = {};
-Object.assign(user10, user8, user9);
-console.log(user10);
+let purchase = {
+  date: new Date(), // Дата покупки
+  customer: 'Tomas', // Клиент
+  items: [ // Массив товаров
+    { name: 'Книга', quantity: 2, price: 100 },
+    { name: 'Ручка', quantity: 5, price: 10 },
+    { name: 'Тетрадь', quantity: 3, price: 50 }
+  ],
+ 
+  getTotalCost() {
+    let totalCost = 0; // Итоговая стоимость покупки
+
+    for (let item of this.items) {
+      totalCost += item.price * item.quantity;
+    }
+    
+    if (totalCost > 1000) {
+      totalCost *= 0.95;
+    }
+ 
+    return totalCost.toFixed(2);
+  }
+};
+
+
+console.log(Дата покупки: ${purchase.date.toLocaleDateString()});
+console.log(Клиент: ${purchase.customer});
+console.log('Список товаров:');
+for (let item of purchase.items) {
+  console.log(${item.name}: ${item.quantity} шт. по ${item.price} грн);
+}
+console.log(Итоговая стоимость покупки: ${purchase.getTotalCost()} грн);
 */
-
-console.log('-----------------');
-let user8 = {name: 'Vasya'};
-let user9 = {name: 'Tom', age: 20, isMale: true};
-let user10 = {name: "Bob"};
-Object.assign(user10, user8, user9);
-console.log(user10);
-
-// копирование с помощью spread оператора / ... / деструктуризации
-
-let user11 = {name: 'Bob', surname: 'Fox'};
-let user12 = {...user11};
-console.log(user11, user12);
-user11.name = 'Tom';
-console.log(user11, user12);
-
-
-let user13 = {...user11, login: 'bobob', password: 'qwerty'};
-
-
-
-
-
-
