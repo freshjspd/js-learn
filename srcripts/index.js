00f5cc67a02ffb70e64dde8df39de35440738e6e
freshjspd <freshjs.pd@gmail.com>
2023-03-04 10:29:13 +0200

    object

--- srcripts/index.js
+++ srcripts/index.js
@@ -1,97 +1,128 @@
 /*
-tasks
-
-1. Подсчитать сумму по модулю элементов массива
-2. Вывести индексы отрицальтелых элементов массива
-3. Вывести колиство отрицательных элементов маасива
-4. Даны два массива. Обьединить и преобразовать в строку
------------------------------------------------------
-5. Отфильтровать массив удалив все отрицательные элементы
-================================================
-6. Предложение превратить в название переменной в стиле camel case
-"my new variable" => myNewVariable
-7. удалить все знаки из предложения (оставить только буквы и цифры
+let x;
+const RED = "#ff0000";
+[5, 0, undefined, {}, {name: 'Vasya'}, function(){}, 'hello', true, [1,2,[100,200],3,4], ,,,,0, null]
+[1,2,3,4,5]
 */
-function getRandomArray(maxValue, countArray){
-    let arr = new Array(countArray);
-    arr.forEach(el => Math.floor(Math.random() * maxValue));
-    /*
-    let arr = [];
-    for(let i=0; i<countArray; i++){
-        arr[i] = Math.floor(Math.random() * maxValue);
+
+let user1 = {};
+//let user1 = new Object();
+console.log(user1);
+
+let user2 = {
+    name: "John",
+    surname: "Smith",
+    age: 20,
+    isMale: true
+};
+console.log(user2);
+
+let user3 = {};
+user3.login = 'vasya1111';
+user3.password = 'qwerty';
+console.log(user3);
+
+let barsik = {
+    name: "Barsik",
+    age: 3,
+    color: "white"
+};
+console.log("Hello, ", barsik.name);
+
+let userName = "Ann Smith";
+let userAge = 20;
+let userLogin = "ann777";
+
+let user4 = {userName, userLogin, userAge};
+let user5 = {userName, userLogin, userAge, role: "web design"};
+
+let car = {
+    model: "modelX",
+    year: 2020
+};
+
+let user6 = {name: 'Vasya', car};
+
+// оператор ... называется деструктуризация
+// раскладываем обьект  / смотрим то что внутри 
+
+let user7 = {...user4, password: 'qwerty', email: 'ann@mail.com'};
+
+let user8 = {
+    login: 'tom111',
+    password: 'qwerty',
+    personal: {
+        name: "Tom",
+        surname: "Adson",
+        address: "UK, London, Main Str., 54/2",
+        tel: "23462423000023"
+    },
+    langs: ["ua", "eng", "es"]
+};
+
+console.log("user: ", user8.login);
+console.log("personal info: ", user8.personal);
+console.log("home address: ", user8.personal.address);
+console.log("langs: ", user8.langs);
+
+// добавить должность staff  и рабочий стаж  exp свойства
+// добавить обьект машина 
+// *добавить в персональной информации дату рождения
+// *добавить в языках новый язык
+// ** удалить любой язык из массива
+
+user8.exp = 5;
+user8.staff = "designer";
+user8.car = {};
+user8.personal.birthDay = "11.12.1995";
+user8.langs.push("Japan");
+user8.langs.splice(2,1);
+console.log(user8);
+
+user8.car = {
+    model: 'modexXXX',
+    year: 2022,
+    color: 'black',
+    engine: {
+        year: 2020,
+        name: 'zxsew34'
     }
-    */
-    console.log(arr);
-    return arr;
-    
-}
+};
 
-function task1(arr){
-    let sum =0 ;
-    for(let i=0; i<arr.length; i++){
-        sum += arr[i];
-    }
-    return Math.abs(sum);
-}
+/*
+user8.car.year
+user8.car.engine.year
 
-function task2(arr){
-    let result = [];
-    for(let i =0, j=0 ; i<arr.length; i++){
-        if(arr[i] < 0){
-            result[j] = i;
-            j++;
-        }
-    }
-    return result;
-}
+user8.car.engina.name = "superengine111"
 
-function task3(arr){
-    let j=0;
-    for(let i =0; i<arr.length; i++){
-        if(arr[i] < 0){
-            j++;
-        }
-    }
-    return j;
-}
-
-function task4(arr1, arr2){
-    for(let i=0; i < arr2.length; i++){
-        arr1.push(arr2[i]);
-    }
-    return arr1.toString();
-}
-
-function task5(arr){
-    let result = [];
-    for(let i=0, j=0; i<arr.length; i++){
-        if(arr[i] < 0) continue;
-        result[j] = arr[i];
-        j++;
-    }
-}
-
-let a = [1, 2, -5, 2, 0, -1, -2, 9 , 0, -4, 1];
-
-let res1 = Math.abs(a.reduce((sum, el) => sum + el));
-
-a.forEach(el => (el<0) ? console.log(el) : el );
-
-let res3 = a.reduce((sum, el) => (el<0) ? sum + el : sum);
-console.log('task3=',res3);
-
-let res5 = a.filter(el => el>0);
-
-let res5_2 = a.forEach((el, i) => (el <=0 ) ? a.splice(i,1): el);
-console.log(a);
+*/
 
 
-let str;
-result = str.split(' ').map(el => el[0].toUpperCase()+ el.slice(1)).join();
+let carTest = {
+    "model": "modexXXX",
+    "year": 2022,
+    "color": 'black'
+};
 
+let carTest2 = {
+    model: "modexXXX",
+    year: 2022,
+    color: 'black'
+};
 
-let res7 = str.split('').forEach(el => (
-    el.charCodeAt([0]) >=48 && el.charCodeAt([0])) <=122 
-    ? console.log(el) 
-    : el
-);
+// массив обьектов
+
+let users = [
+    {name: 'name1', surname: 'surname1'}, 
+    {name: 'name2', surname: 'surname2'}, 
+    {name: 'name3', surname: 'surname3'}, 
+    {name: 'name4', surname: 'surname4'}, 
+];
+
+let users2 = [
+    user2,
+    user3,
+    user4,
+    user6,
+    user8
+];
