fcaf6c7dcab99459cfcc406107b0a6274ca2896f
freshjspd <freshjs.pd@gmail.com>
2023-03-11 13:19:03 +0200

    object proto

--- srcripts/index.js
+++ srcripts/index.js
@@ -1,82 +1,20 @@
-// деструктуризация массивов
-const arr = ["val1", "val2", "val3", "val4", "val5"];
-const arr0 = arr[0];
-const arr1 = arr[1];
-//const [a0, a1] = arr;
-//const [a0, a1,,a3] = arr;
-//const [a0, ...rest] = arr;
-const[a0,,a2,...rest] = arr; //a0-val1, a2-val3, rest - [val4,val5]
-
-const arr2 = ["v1", [10,20,30,40,50], "v2", "v3", "v4", "v5"];
-
-//const [,result] = arr2;
-//console.log(result);
-
-const[,[,,result]] = arr2;
-
-// деструктуризация обьектов
-
 const user = {
-    name: 'Vasya',
-    surname: 'Test',
-    car: {
-        model: 'modelX',
-        color: 'black',
-        year: 2020,
-        engine: {
-            power: 2,
-            year: 2019
-        }
-    }
+    login: 'anon',
+    password: 'admin',
+    authData: function(){
+        console.log('Auth data:', this.login, this.password);
+    } 
 };
 
-//user.name;
-
-const {name} = user;
-console.log(name);
-const {firstName, lastName} = user;
-console.log(firstName, lastName);
-
-//const {car:{engine: power}} = user;
-const {car: model} = user;
-
-
-const obj1 = {
-    name: 'Tom',
-    surname: 'Adson'
+const stud = {
+    fullname: 'Tom Adson'
 };
 
-const obj2 = {
-    name: 'Nike',
-    age: 20,
-}
+stud.__proto__ = user;
 
-const Tom = {
-    ...obj1,
-    ...obj2
-};
+//let stud = Object.create(user);
 
-
-class User{
-    constructor(login, password){
-        this.login = login;
-        this.password = password;
-    }
-};
-
-class Stud extends User{
-    constructor(name, surname, auth){
-        const {login, password} = auth;
-        super(login, password);
-        this.name = name;
-        this.surname = surname;
-    }
-};
-
-
-const stud1 = new Stud('Tom', 'Fox', {login:'tom1', password:'qwerty'});
-
-
-
-
-
+console.log(stud.fullname);
+console.log(stud.login); 
+console.log(stud.password);
+stud.authData(); 
