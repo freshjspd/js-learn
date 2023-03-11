e7e007a751fbbcf3f059b5917f080b410141fe75
freshjspd <freshjs.pd@gmail.com>
2023-03-11 13:36:39 +0200

    карта прав доступа

--- srcripts/index.js
+++ srcripts/index.js
@@ -1,20 +1,35 @@
-const user = {
-    login: 'anon',
-    password: 'admin',
-    authData: function(){
-        console.log('Auth data:', this.login, this.password);
-    } 
+// 1 обьект ROLE - admin, moderator, user: 'user',
+// 2 обьект ACTION - create, read, update, delete
+
+// создать карту MAP 
+
+//функцию которая проверяет права доступа
+
+// ***** класс User login, role
+
+const ROLE = {
+    ADMIN: 'ADMIN',
+    MODERATOR: 'MODERATOR',
+    USER: 'USER'
 };
 
-const stud = {
-    fullname: 'Tom Adson'
+const ACTION = {
+    CREATE: 'CREATE',
+    READ: 'READ',
+    UPDATE: 'UPDATE',
+    DELETE: 'DELETE'
 };
 
-stud.__proto__ = user;
+let mapRights = new Map();
 
-//let stud = Object.create(user);
+mapRights.set(ROLE.USER, [ACTION.READ]);
+mapRights.set(ROLE.MODERATOR, [ACTION.CREATE, ACTION.READ, ACTION.UPDATE]);
+//mapRights.set(ROLE.ADMIN, [ACTION.CREATE, ACTION.READ, ACTION.UPDATE, ACTION.DELETE]);
+mapRights.set(ROLE.ADMIN, ...ACTION);
 
-console.log(stud.fullname);
-console.log(stud.login); 
-console.log(stud.password);
-stud.authData(); 
+function checkPermision(role, action){
+    if(mapRights.has(role)){
+        return mapRights.get(role).includes(action);
+    }
+    return false;
+}
