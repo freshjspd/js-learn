b309a245eee29272246eae71746b724c6828cbc9
freshjspd <freshjs.pd@gmail.com>
2023-04-29 11:31:05 +0300

    async load data: fetch

--- srcripts/index.js
+++ srcripts/index.js
@@ -1,43 +1,18 @@
-/*
-const user = {
-    name: 'John',
-    surname: 'Adson',
-    age: 20,
-    isMale: true
-};
-
-const resUser = JSON.stringify(user);
-
-const gettingUser = JSON.parse(resUser);
-*/
-
-//Задача: асинхр. считать массив пользователей
-
-const store = {
-    users: null,
-    error: null,
-    isFetching: false   //isLoading
-};
-
 const btn = document.getElementById('loadData');
-const request = new XMLHttpRequest();
 
 btn.onclick = function(){
-    request.open("GET", "../users.json", false);
-    request.send();
+    fetch('../users.json').then(loadData).then(printData).catch(loadError);
 }
 
-request.onloadstart = function(){ 
-    store.isFetching = true; 
-    console.log(store);
-};
+function loadData(res){
+    return res.json();
+}
 
-request.onloadend = function(){
-    store.isFetching = false;
-    if(this.status >= 200 && this.status <300){
-        store.users = JSON.parse(this.responseText);
-    } else{
-        store.error = new Error(`Loading Error. code ${this.status} - ${this.statusText}`);
-    }
-};
+function printData(data){
+    console.log(data);
+}
+
+function loadError(res){
+    console.log(res);
+}
 
