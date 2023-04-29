5c9840271964b6bbd492d4cfd13b6583ceb2f625
freshjspd <freshjs.pd@gmail.com>
2023-04-29 09:33:24 +0300

    example multi-tasks for async

--- srcripts/index.js
+++ srcripts/index.js
@@ -1,35 +1,15 @@
-// 1 обьект ROLE - admin, moderator, user: 'user',
-// 2 обьект ACTION - create, read, update, delete
+'use strict';
 
-// создать карту MAP 
+const square = document.querySelector('.item');
+let deg = 0;
+setInterval(foo, 1000/60);
 
-//функцию которая проверяет права доступа
+function foo(){
+    square.style.transform=`rotate(${deg+=10}deg)`
+}
 
-// ***** класс User login, role
+const btn = document.querySelector('button');
 
-const ROLE = {
-    ADMIN: 'ADMIN',
-    MODERATOR: 'MODERATOR',
-    USER: 'USER'
-};
-
-const ACTION = {
-    CREATE: 'CREATE',
-    READ: 'READ',
-    UPDATE: 'UPDATE',
-    DELETE: 'DELETE'
-};
-
-let mapRights = new Map();
-
-mapRights.set(ROLE.USER, [ACTION.READ]);
-mapRights.set(ROLE.MODERATOR, [ACTION.CREATE, ACTION.READ, ACTION.UPDATE]);
-//mapRights.set(ROLE.ADMIN, [ACTION.CREATE, ACTION.READ, ACTION.UPDATE, ACTION.DELETE]);
-mapRights.set(ROLE.ADMIN, ...ACTION);
-
-function checkPermision(role, action){
-    if(mapRights.has(role)){
-        return mapRights.get(role).includes(action);
-    }
-    return false;
+btn.onclick = function(){
+    alert('Hello!');
 }
